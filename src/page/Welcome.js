04a6895d00193ab4f7b8a4e78f06f5aa88e22cc3
Nikhil3522 <nikhiljha3522@gmail.com>
2023-06-17 18:31:31 +0530

    AOS Animation

--- src/page/Welcome.js
+++ src/page/Welcome.js
@@ -1,19 +1,27 @@
 import { useEffect, useState } from "react";
 import ButtonComponent from "../component/ButtonComponent";
 import Banner from '../assets/images/banner1.png';
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function Welcome(){
-
+    useEffect(() => {
+      Aos.init({duration: 600})
+    }, [])
     return(
         <div>
-          <img src={Banner} width="300px"/>
-          <ButtonComponent 
-            title="LOGIN"
-          />
-          <ButtonComponent 
-            title="SIGN UP"
-          />
+          <img src={Banner} width="300px" data-aos="zoom-in-up"/>
+          <div  data-aos="fade-left">
+            <ButtonComponent 
+              title="LOGIN"
+            />
+          </div>
 
+          <div data-aos="fade-right">
+            <ButtonComponent 
+              title="SIGN UP"
+            />
+          </div>
         </div>
     )
 }
