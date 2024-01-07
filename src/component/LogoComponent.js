b3626ff0977d859116bea1db7fce36021bb87d95
Nikhil3522 <nikhiljha3522@gmail.com>
2024-01-07 19:50:16 +0530

    Logo, chat bg

--- src/component/LogoComponent.js
+++ src/component/LogoComponent.js
@@ -1,10 +1,10 @@
-import logo from '../assets/images/logo.png'
+import logo from '../assets/logo.png'
 
 function LogoComponent(){
     return (
         <div className="title">
-            <img src={logo} width="45px" height="45px"/>
-            <h3>title</h3>
+            <img src={logo} height="45px"/>
+            {/* <h3>title</h3> */}
         </div>
     )
 }
