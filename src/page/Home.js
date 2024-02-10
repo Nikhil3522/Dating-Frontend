8f73d1070ef37b71aa51aae9e5cbcecdc7dbd637
Nikhil3522 <nikhiljha3522@gmail.com>
2024-02-10 17:55:26 +0530

    Bug Fixed: (Cannot read property 'div' of undefined)

--- src/page/Home.js
+++ src/page/Home.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // import "./tinderCard.css"
 import '../App.css';
 import NavigationBar from "../component/NavigationBar";
-import ProfileCard from "../component/ProfileCard";
+// import ProfileCard from "../component/ProfileCard";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import verified from '../assets/icons/verified.png';
