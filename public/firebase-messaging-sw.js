1bc7923776c50392a0d74a2be9345f6573545f0c
Nikhil3522 <nikhiljha3522@gmail.com>
2024-02-10 18:36:12 +0530

    firebase messaging

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,10 +1,31 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
-);
+// Scripts for firebase and firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 
-firebase.initializeApp(
-  JSON.parse(new URL(location).searchParams.get("firebaseConfig"))
-);
+// Initialize the Firebase app in the service worker by passing the generated config
+const firebaseConfig = {
+  apiKey: "AIzaSyDzR07XPZcQmGA93xwTxy7R4Gs9R6kI8HE",
+  authDomain: "fir-77add.firebaseapp.com",
+  projectId: "https://fir-77add-default-rtdb.asia-southeast1.firebasedatabase.app",
+  storageBucket: "fir-77add",
+  messagingSenderId: "578008944252",
+  appId: "1:578008944252:web:2cbaac4236018103a143f9v",
+  measurementId: "G-N5YGG8H9FH",
+};
 
-firebase.messaging();
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
+ // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import './App.css';
-import Welcome from './page/Welcome';
 import LogoComponent from './component/LogoComponent';
 import Navigator from './route/Navigator';
 
 function App() {
 
-  if ("serviceWorker" in navigator) {
-    console.log("Registration started");
-    const firebaseConfig = encodeURIComponent(
-      JSON.stringify({
-        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.REACT_APP_FIREBASE_API_ID,
-      })
-    );
-    navigator.serviceWorker
-      .register(
-        `../../../firebase-messaging-sw.js?firebaseConfig=${firebaseConfig}`
-      )
-      .then(function (registration) {
-        console.log("Registration successful, scope is:", registration.scope);
-      })
-      .catch(function (err) {
-        console.log("Service worker registration failed, error:", err);
-      });
-    }
+  // if ("serviceWorker" in navigator) {
+  //   console.log("Registration started");
+  //   const firebaseConfig = encodeURIComponent(
+  //     JSON.stringify({
+  //       apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  //       projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  //       messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  //       appId: process.env.REACT_APP_FIREBASE_API_ID,
+  //     })
+  //   );
+  //   navigator.serviceWorker
+  //     .register(
+  //       `../../../firebase-messaging-sw.js?firebaseConfig=${firebaseConfig}`
+  //     )
+  //     .then(function (registration) {
+  //       console.log("Registration successful, scope is:", registration.scope);
+  //     })
+  //     .catch(function (err) {
+  //       console.log("Service worker registration failed, error:", err);
+  //     });
+  //   }
     
   return (
     <div className="App">
